1c9fd46461b45c214e3dd902dc28c0e3e63c7ee4
ysndy <ysndy@naver.com>
2022-11-23 17:19:00 +0900

    proxi 설정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+
+  devServer: {
+    // 프록시 설정
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
+
 })
